7e459da0468e9702522e087b9bf5c9df3113d236
Onni Kukkonen <onni.kukkonen77@gmail.com>
2024-02-12 23:19:18 +0200

    remove unused content from this repo

--- OpenSteamworks.Native/htmlhost/ghidradefines.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-typedef unsigned char   undefined;
-typedef unsigned char    undefined1;
-typedef unsigned short    undefined2;
-typedef unsigned int    undefined4;
-typedef unsigned long long    undefined8;
-typedef int unknown_ret;
-typedef unsigned int uint;
--- OpenSteamworks.Native/protobufhack/export.h
+++
@@ -1,7 +0,0 @@
-#ifdef _WIN32
-#define EXPORT extern "C" __declspec(dllexport)
-#endif
-
-#if __linux__
-#define EXPORT extern "C"
-#endif
